c1e41b7dd3676c8b1057fa5e821892b61b09563e
chien <sarahxoxo@users.noreply.github.com>
2017-10-22 15:18:29 +0800

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+## calculator-style-hacktober
+
+This is a simple calculator.
+
+You can style it and make a pull request.
+
+
+#### Please follow the rules below for a successful pull request and star it if you like this project.
+
+
+## how to contribute
+
+- add a new `css file` in the css folder to get started
+- add any style you like for the calculator 
+- add your `css file's name` at `line 38` in `index.html` file
+
+  (for example: `var styles = ["index.css","index2.css"];`)
+- make a pull request
---
+++ index.js
@@ -0,0 +1,60 @@
+$(function() {
+  var result = 0;
+  var operation = null;
+  var currentEntry = 0;
+  var prevEntry = 0;
+  updateScreen(result);
+  
+  $(".button").on("click", function(e) {
+    var pressed = $(this).data("value");
+
+    if(pressed === "C") {
+      result = 0;
+      currentEntry = 0;
+      updateScreen(currentEntry);
+    } else if(pressed === "+/-") {
+      currentEntry *= -1;
+      updateScreen(currentEntry);
+    } else if(pressed === "%") {
+      currentEntry /= 100;
+      updateScreen(currentEntry);
+    } else if(isOperator(pressed)) {
+      prevEntry = currentEntry;
+      operation = pressed;
+      currentEntry = "";
+      $(".result span").html(operation);
+    } else if(isNum(pressed)) {
+      if(currentEntry === 0) {
+        currentEntry = pressed;
+      } else {
+        currentEntry += pressed.toString();
+      }
+      updateScreen(currentEntry);
+    } else if(pressed === "=") {
+      currentEntry = compute(prevEntry, currentEntry, operation);
+      operation = null;
+      updateScreen(currentEntry);
+    }
+  });
+});
+
+function updateScreen(display) {
+  $(".result span").html(display);
+}
+
+function isNum(value) {
+  return !isNaN(value);
+} 
+
+function isOperator(value) {
+  return value === "÷" || value === "x" || value === "-" || value === "+";
+}
+function compute(prev, current, operation) {
+  prev = parseFloat(prev);
+  current = parseFloat(current);
+  
+  if(operation === "+") return prev + current;
+  else if(operation === "-") return prev - current;
+  else if(operation === "x") return prev * current;
+  else if(operation === "÷") return prev / current;
+}
